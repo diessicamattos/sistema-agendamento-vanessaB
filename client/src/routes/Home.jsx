c186ba77b3e1bd511c880484a95ac05d32277933
Diéssica Mattos <164754833+diessicamattos@users.noreply.github.com>
2025-08-26 19:16:49 -0300

    atualizacao imagens

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-//src/routes/Home.jsx
+// src/routes/Home.jsx
 
 import React, { useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
@@ -14,19 +14,19 @@ export default function Home() {
 
   // Serviços
   const services = [
-    { id: 1, name: "Pé e mão tradicional", price: 62, duration: "2 horas", image: "./public/alongamento.jpg" },
-    { id: 2, name: "Mão", price: 30, duration: "1 hora", image: "./public/alongamento.jpg" },
-    { id: 3, name: "Pé", price: 36, duration: "1 hora", image: "./public/alongamento.jpg" },
-    { id: 4, name: "Banho de gel", price: 150, duration: "1h30min", image: "./public/alongamento.jpg" },
-    { id: 5, name: "Manutenção 15 dias (Gel)", price: 85, duration: "1h30min", image: "./public/alongamento.jpg" },
-    { id: 6, name: "Manutenção 21 dias (Gel)", price: 100, duration: "1h30min", image: "./public/alongamento.jpg" },
-    { id: 7, name: "Aplicação Alongamento", price: 180, duration: "2h30min", image: "./public/alongamento.jpg" },
-    { id: 8, name: "Manutenção 15 dias (Alongamento)", price: 85, duration: "1 hora", image: "./public/alongamento.jpg" },
-    { id: 9, name: "Manutenção 21 dias (Alongamento)", price: 100, duration: "1h30min", image: "./public/alongamento.jpg" },
-    { id: 10, name: "Retoque de unha quebrada", price: 10, duration: "30 min", image: "./public/alongamento.jpg" },
-    { id: 11, name: "Par de decoração", price: 10, duration: "30 min", image: "./public/alongamento.jpg" },
-    { id: 12, name: "Encapsulada", price: 20, duration: "30 min", image: "./public/alongamento.jpg" },
-    { id: 13, name: "Esmaltação em gel", price: 70, duration: "30 min", image: "./public/alongamento.jpg" },
+    { id: 1, name: "Pé e mão tradicional", price: 62, duration: "2 horas", image: "/alongamento.jpg" },
+    { id: 2, name: "Mão", price: 30, duration: "1 hora", image: "/alongamento.jpg" },
+    { id: 3, name: "Pé", price: 36, duration: "1 hora", image: "/alongamento.jpg" },
+    { id: 4, name: "Banho de gel", price: 150, duration: "1h30min", image: "/alongamento.jpg" },
+    { id: 5, name: "Manutenção 15 dias (Gel)", price: 85, duration: "1h30min", image: "/alongamento.jpg" },
+    { id: 6, name: "Manutenção 21 dias (Gel)", price: 100, duration: "1h30min", image: "/alongamento.jpg" },
+    { id: 7, name: "Aplicação Alongamento", price: 180, duration: "2h30min", image: "/alongamento.jpg" },
+    { id: 8, name: "Manutenção 15 dias (Alongamento)", price: 85, duration: "1 hora", image: "/alongamento.jpg" },
+    { id: 9, name: "Manutenção 21 dias (Alongamento)", price: 100, duration: "1h30min", image: "/alongamento.jpg" },
+    { id: 10, name: "Retoque de unha quebrada", price: 10, duration: "30 min", image: "/alongamento.jpg" },
+    { id: 11, name: "Par de decoração", price: 10, duration: "30 min", image: "/alongamento.jpg" },
+    { id: 12, name: "Encapsulada", price: 20, duration: "30 min", image: "/alongamento.jpg" },
+    { id: 13, name: "Esmaltação em gel", price: 70, duration: "30 min", image: "/alongamento.jpg" },
   ]
 
   function handleAgendar(service) {
@@ -40,7 +40,7 @@ export default function Home() {
       {/* Banner */}
       <section className="flex flex-col md:flex-row items-center bg-[#000001] p-4 md:p-6 rounded-2xl shadow-lg gap-4">
         <img 
-          src="./public/proprietaria.jpg" 
+          src="/proprietaria.jpg" 
           alt="prestadora" 
           className="w-full md:w-1/2 h-auto rounded-2xl object-cover border-4 border-[#D7AF70]" 
         />

2df8419716b481b4668b5cdb7d383174400c7f9c
Diéssica Mattos <164754833+diessicamattos@users.noreply.github.com>
2025-08-29 23:59:49 -0300

    ajuste nas sessões de serviços

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 //src/main.jsx
 
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
@@ -12,4 +12,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
+)*/
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW() // registra o service worker
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})
+*/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Meu App',
+        short_name: 'App',
+        description: 'Meu sistema em Vite + React como PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
